2209392c8d1e149897d5210dcae5faf1593e6613
Silvia García Cacho <silvia@garca.info>
2019-03-06 12:23:03 +0100

    Add JSON storage of API requests

--- src/App.js
+++ src/App.js
@@ -2,6 +2,51 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      pokemonList: [],
+    }
+
+    this.getSavedData = this.getSavedData.bind(this);
+    this.getData = this.getData.bind(this);
+  }
+
+  componentDidMount(){
+    this.getSavedData();
+  }
+
+  getSavedData(){
+    if(localStorage.getItem('pokemonList') !== null){
+      let mySavedData = JSON.parse(localStorage.getItem('pokemonList'));
+      this.setState({
+        pokemonList: mySavedData,
+      })
+    } else {
+      this.getData();
+    }
+  }
+
+  getData(){
+    //https://pokeapi.co/api/v2/pokemon/{id or name}/
+    const endpoint = 'https://pokeapi.co/api/v2/pokemon/';
+    fetch(endpoint)
+      .then(res=>res.json())
+      .then(data=> {
+        console.log(data);
+        let results = data.results;
+        this.setState({
+          pokemonList: results,
+        });
+        this.saveData(this.state.pokemonList, 'pokemonList');
+        }
+      ); 
+  }
+
+  saveData(data, dataName){
+    localStorage.setItem(dataName, JSON.stringify(data))
+  }
+  
   render() {
     return (
       <div className="App">
