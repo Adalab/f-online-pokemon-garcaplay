8c869511ee754c1c97e5ee48d8d6bca070a4b2ac
Silvia García Cacho <silvia@garca.info>
2019-03-10 21:15:52 +0100

    Create docs folder

---
+++ docs/precache-manifest.85f423e017990bf37a7cc7553d74d008.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "55c065a7ae913ce689f1",
+    "url": "./static/js/main.55c065a7.chunk.js"
+  },
+  {
+    "revision": "935774e34e014106f98c",
+    "url": "./static/js/2.935774e3.chunk.js"
+  },
+  {
+    "revision": "55c065a7ae913ce689f1",
+    "url": "./static/css/main.9d48c50f.chunk.css"
+  },
+  {
+    "revision": "68bab59bdd1be08aa2606ac089f085a5",
+    "url": "./index.html"
+  }
+];
