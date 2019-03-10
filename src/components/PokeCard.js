4f98be856974229d6eeab140ed5ab491365bbe9c
Silvia García Cacho <silvia@garca.info>
2019-03-10 21:12:15 +0100

    Insert colors to style and keep trying with the header-main-footer thing

--- src/components/PokeCard.js
+++ src/components/PokeCard.js
@@ -9,28 +9,28 @@ class PokeCard extends Component {
                 filteredPokemons.map((poke, index)=>{
                     return(
                         <li className="List__item" key={index}>
-                        <div className="List__item-card">
-                            <div className="Card__header">
-                            <div className="Card__id">
-                                ID/#{poke.id}
+                            <div className="List__item-card">
+                                <div className="Card__header">
+                                    <div className="Card__id">
+                                        ID/#{poke.id}
+                                    </div>
+                                    <img src={poke.sprites.front_default} className="Card__img" alt={poke.name}/>
+                                </div>
+                                <div className="Card__body">
+                                    <h2 className="Card__title">{poke.name}</h2>
+                                    <div className="Card__chips">
+                                        <ul className="Card__chips-list">
+                                            {poke.types.map((chip, index)=>{
+                                                return(
+                                                    <li className="Chips__list-item" key={index}>
+                                                        <h3>{chip.type.name}</h3>
+                                                    </li>
+                                                )
+                                            })}
+                                        </ul>
+                                    </div>
+                                </div>
                             </div>
-                            <img src={poke.sprites.front_default} className="Card__img" alt={poke.name}/>
-                            </div>
-                            <div className="Card__body">
-                            <h2 className="Card__title">{poke.name}</h2>
-                            <div className="Card__chips">
-                                <ul className="Card__chips-list">
-                                    {poke.types.map((chip, index)=>{
-                                        return(
-                                        <li className="Chips__list-item" key={index}>
-                                            <h3>{chip.type.name}</h3>
-                                        </li>
-                                        )
-                                    })}
-                                </ul>
-                            </div>
-                            </div>
-                        </div>
                         </li>
                     )
                 })
